6b529bc3fa7a2eaeebc8177988e130f476efa763
henny0912 <2024359@student.cct.ie>
2025-04-16 11:51:53 +0100

    minor change

--- src/cakeapp/CakeAppGUI.java
+++ src/cakeapp/CakeAppGUI.java
@@ -15,6 +15,7 @@ public class CakeAppGUI {
     public static void main(String[] args) {
         // TODO code application logic here
         CakeGUI guicakeapp = new CakeGUI();
+        
         guicakeapp.setVisible(true);
     }
 }
